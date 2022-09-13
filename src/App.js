0668d56b2d9a6d6a845806c36e710f3856ee09fa
Gregor Gilchrist <gregorgilchrist@gmail.com>
2022-09-13 21:12:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const api = {
+  key: "8e8a5629885d66a0857172614fc0f5bd",
+  baseApiUrl: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
