86412b65095a7c5596ea0abe23fb9e5d9de60eb6
Gregor Gilchrist <gregorgilchrist@gmail.com>
2022-09-29 20:25:42 +0200

    moved weather logic and elements to it's own component WeatherInfoComponent

--- packages/mobileWeatherApp/App.tsx
+++ packages/mobileWeatherApp/App.tsx
@@ -119,45 +119,6 @@ function App() {
     setBackgroundImageUrl(backgroundUrl);
   }, [weather]);
 
-  const dayBuilder = d => {
-    let days = [
-      'Sunday',
-      'Monday',
-      'Tuesday',
-      'Wednesday',
-      'Thursday',
-      'Friday',
-      'Saturday',
-    ];
-
-    let day = days[d.getDay()];
-
-    return `${day}`;
-  };
-
-  const dateBuilder = d => {
-    let months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ];
-
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${date} ${month} ${year}`;
-  };
-
   return (
     <View style={{flex: 1, fontFamily: 'Avenir'}}>
       <ImageBackground
@@ -211,7 +172,6 @@ function App() {
             Get current weather!
           </Text>
         </TouchableOpacity>
-        <WeatherInfoContainer />
         {typeof weather.cod === 'string' ? (
           <View
             style={{
@@ -230,105 +190,7 @@ function App() {
         ) : null}
 
         {typeof weather.main !== 'undefined' ? (
-          <View
-            style={{
-              flex: 1,
-              width: '100%',
-              marginTop: '20%',
-              backgroundColor: 'rgba(255, 255, 255, 0.1)',
-            }}>
-            <View style={{marginLeft: 5}}>
-              <View>
-                <Text style={{fontSize: 20, fontFamily: 'Avenir'}}>
-                  {dayBuilder(new Date())}
-                </Text>
-                <Text
-                  style={{
-                    marginBottom: 10,
-                    fontSize: 20,
-                    fontFamily: 'Avenir',
-                  }}>
-                  {dateBuilder(new Date())}
-                </Text>
-                <Text style={{fontSize: 40, fontFamily: 'Avenir'}}>
-                  {weather.name}, {weather.sys.country}
-                </Text>
-              </View>
-              <Text style={{fontSize: 100, fontFamily: 'Avenir'}}>
-                {Math.round(weather.main.temp)}°C
-              </Text>
-              <Text style={{fontSize: 40, fontFamily: 'Avenir'}}>
-                {weather.weather[0].main}
-              </Text>
-            </View>
-            <View
-              style={{
-                flex: 1,
-                flexDirection: 'row',
-                marginBottom: 50,
-                alignSelf: 'center',
-                position: 'absolute',
-                bottom: 1,
-                width: '80%',
-                backgroundColor: 'rgba(255,255,255,0.2)',
-                borderRadius: 10,
-              }}>
-              <View style={{flex: 1, alignItems: 'center'}}>
-                <Text
-                  style={{
-                    fontSize: 30,
-                    color: 'rgba(255,255,255,0.8)',
-                    fontFamily: 'Avenir',
-                  }}>
-                  {Math.round(weather.main.feels_like)}°C
-                </Text>
-                <Text
-                  style={{
-                    fontSize: 18,
-                    color: 'rgba(255,255,255,0.8)',
-                    fontFamily: 'Avenir',
-                  }}>
-                  Feels like
-                </Text>
-              </View>
-              <View style={{flex: 1, alignItems: 'center'}}>
-                <Text
-                  style={{
-                    fontSize: 30,
-                    color: 'rgba(255,255,255,0.8)',
-                    fontFamily: 'Avenir',
-                  }}>
-                  {Math.round(weather.wind.speed)} m/s
-                </Text>
-                <Text
-                  style={{
-                    fontSize: 18,
-                    color: 'rgba(255,255,255,0.8)',
-                    fontFamily: 'Avenir',
-                  }}>
-                  Wind
-                </Text>
-              </View>
-              <View style={{flex: 1, alignItems: 'center'}}>
-                <Text
-                  style={{
-                    fontSize: 30,
-                    color: 'rgba(255,255,255,0.8)',
-                    fontFamily: 'Avenir',
-                  }}>
-                  {Math.round(weather.main.humidity)}%
-                </Text>
-                <Text
-                  style={{
-                    fontSize: 18,
-                    color: 'rgba(255,255,255,0.8)',
-                    fontFamily: 'Avenir',
-                  }}>
-                  Humidity
-                </Text>
-              </View>
-            </View>
-          </View>
+          <WeatherInfoContainer weather={weather} />
         ) : undefined}
       </ImageBackground>
     </View>
--- packages/mobileWeatherApp/src/components/WeatherInfoContainer.tsx
+++ packages/mobileWeatherApp/src/components/WeatherInfoContainer.tsx
@@ -1,7 +1,146 @@
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 
-const WeatherInfoContainer = () => {
-  return <Text>BLASHDASDH</Text>;
+const WeatherInfoContainer = props => {
+  const dayBuilder = d => {
+    let days = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+
+    let day = days[d.getDay()];
+
+    return `${day}`;
+  };
+
+  const dateBuilder = d => {
+    let months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${date} ${month} ${year}`;
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        width: '100%',
+        marginTop: '20%',
+        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+      }}>
+      <View style={{marginLeft: 5}}>
+        <View>
+          <Text style={{fontSize: 20, fontFamily: 'Avenir'}}>
+            {dayBuilder(new Date())}
+          </Text>
+          <Text
+            style={{
+              marginBottom: 10,
+              fontSize: 20,
+              fontFamily: 'Avenir',
+            }}>
+            {dateBuilder(new Date())}
+          </Text>
+          <Text style={{fontSize: 40, fontFamily: 'Avenir'}}>
+            {props.weather.name}, {props.weather.sys.country}
+          </Text>
+        </View>
+        <Text style={{fontSize: 100, fontFamily: 'Avenir'}}>
+          {Math.round(props.weather.main.temp)}°C
+        </Text>
+        <Text style={{fontSize: 40, fontFamily: 'Avenir'}}>
+          {props.weather.weather[0].main}
+        </Text>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          marginBottom: 50,
+          alignSelf: 'center',
+          position: 'absolute',
+          bottom: 1,
+          width: '80%',
+          backgroundColor: 'rgba(255,255,255,0.2)',
+          borderRadius: 10,
+        }}>
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <Text
+            style={{
+              fontSize: 30,
+              color: 'rgba(255,255,255,0.8)',
+              fontFamily: 'Avenir',
+            }}>
+            {Math.round(props.weather.main.feels_like)}°C
+          </Text>
+          <Text
+            style={{
+              fontSize: 18,
+              color: 'rgba(255,255,255,0.8)',
+              fontFamily: 'Avenir',
+            }}>
+            Feels like
+          </Text>
+        </View>
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <Text
+            style={{
+              fontSize: 30,
+              color: 'rgba(255,255,255,0.8)',
+              fontFamily: 'Avenir',
+            }}>
+            {Math.round(props.weather.wind.speed)} m/s
+          </Text>
+          <Text
+            style={{
+              fontSize: 18,
+              color: 'rgba(255,255,255,0.8)',
+              fontFamily: 'Avenir',
+            }}>
+            Wind
+          </Text>
+        </View>
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <Text
+            style={{
+              fontSize: 30,
+              color: 'rgba(255,255,255,0.8)',
+              fontFamily: 'Avenir',
+            }}>
+            {Math.round(props.weather.main.humidity)}%
+          </Text>
+          <Text
+            style={{
+              fontSize: 18,
+              color: 'rgba(255,255,255,0.8)',
+              fontFamily: 'Avenir',
+            }}>
+            Humidity
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
 };
 
 export default WeatherInfoContainer;
